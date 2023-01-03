fad240b12a5eeb6ba8a989ebe18ae61cde539ef2
Ryan Sharafuddin <>
2023-01-03 07:39:55 -0500

    Came up with a plan

--- Tiling_2023.py
+++ Tiling_2023.py
@@ -243,19 +243,28 @@ def printOutput(tilings):
         print(f"For {WIDTH} x {HEIGHT} rectangles:", file = f)
         print(f"The number of tilings is: {len(tilings)}", file = f)
 
+"""
+    WARN: 
+        The below code for symmetries does not actually work. Consider this: 
+        33:
+       [[2 2 0]
+        [2 0 1]
+        [0 1 1]]
+        Above tiling taken from 3x3. That is the same as:
+       [[1 1 0]
+        [1 0 2]
+        [0 2 2]]   
+        rotated 180 degrees. But current code will not catch b/c 2 != 1.
 
-    # WARN: 
-    #     The below code for symmetries does not actually work. Consider this: 
-    #     33:
-    #    [[2 2 0]
-    #     [2 0 1]
-    #     [0 1 1]]
-    #     Above tiling taken from 3x3. That is the same as:
-    #    [[1 1 0]
-    #     [1 0 2]
-    #     [0 2 2]]   
-    #     rotated 180 degrees. But current code will not catch b/c 2 != 1.    
-
+    IDEA: 
+        Have loop_rec append a tuple to tilings, which includes the original tiling as well as a list of lists (say, l) [[], [], [], []], where each second-level list corresponds to one of the 4 types of L tiles. l[i] will be a list of coordinates of the upper left corners of L tile type i, sorted by being nearest top, then leftmost (upper left is first, then upper right, etc) (so, 1 combo from the filtered_L_tile_locations). These are guaranteed to be exactly the same for the same tiling. Then, make a function that 'rotates/reflects' this representation, since you know that a transformation will transform one type of L tile into another and where its new upper left corner will be. Then, can compare those for symmetries.
+    STEPS:
+        1) Modify loop_rec to attach the needed information, and modify printing to print it out with the tilings.
+        2) Make a function that rotates this representation 90 degrees counterclockwise.
+        3) Make a function that reflects this representation horizontally.
+        4) Modify below 2 symmetry functions to use this representation.
+        5) Print out the filtered tilings (perhaps with a note about which of the original tilings were eliminated and which they were symmetric to)
+"""
 
 # def getTilingsFilteredForSymmetry(tilings):
 #     symmetries_set = set()
